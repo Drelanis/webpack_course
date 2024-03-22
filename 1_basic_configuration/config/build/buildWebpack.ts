eb8ffc898f73f9ac1a452889671d248c0b8a2975
Denys <denisbadakaty51@gmail.com>
2024-03-22 17:48:10 +0200

    Source_map

--- 1_basic_configuration/config/build/buildWebpack.ts
+++ 1_basic_configuration/config/build/buildWebpack.ts
@@ -1,29 +1,32 @@
 import webpack from 'webpack';
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 import { buildDevServer } from './buildDevServer';
 import { buildLoaders } from './buildLoaders';
 import { buildPlugins } from './buildPlugins';
 import { buildResolvers } from './buildResolvers';
 import { BuildOptions } from '../types';
 
-export const buildWebpack = (options: BuildOptions): webpack.Configuration & DevServerConfiguration => {
-  const {mode, paths} = options
+export const buildWebpack = (
+  options: BuildOptions,
+): webpack.Configuration & DevServerConfiguration => {
+  const { mode, paths } = options;
 
-  const isDev = mode === "development";
+  const isDev = mode === 'development';
 
-    return {
-        mode: mode ?? "development",
-        entry: paths.entry,
-        output: {
-            path: paths.output,
-            filename: '[name].[contenthash].js',
-            clean: true,
-        },
-        plugins: buildPlugins(options),
-        module: {
-          rules: buildLoaders(options),
-        },
-        resolve: buildResolvers(options),
-        devServer: isDev ? buildDevServer(options) : undefined,
-      };
-}
+  return {
+    mode: mode ?? 'development',
+    entry: paths.entry,
+    output: {
+      path: paths.output,
+      filename: '[name].[contenthash].js',
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(options),
+    devtool: isDev ? 'eval-cheap-module-source-map' : 'source-map',
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+};
