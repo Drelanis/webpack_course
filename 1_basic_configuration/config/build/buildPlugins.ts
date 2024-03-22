57214f570c53dd12294e3f9c50f128a93884b735
Denys <denisbadakaty51@gmail.com>
2024-03-22 15:24:56 +0200

    tree shaking

--- 1_basic_configuration/config/build/buildPlugins.ts
+++ 1_basic_configuration/config/build/buildPlugins.ts
@@ -1,34 +1,41 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack, { Configuration } from "webpack";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
-import { BuildOptions } from "../types";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack, { Configuration, DefinePlugin } from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { BuildOptions } from '../types';
 
+export const buildPlugins = (
+  options: BuildOptions,
+): Configuration['plugins'] => {
+  const { mode, paths, analyzer, platform } = options;
 
-export const buildPlugins = (options: BuildOptions): Configuration["plugins"] => {
-    const {mode, paths, analyzer} = options
+  const isDev = mode === 'development';
+  const isProd = mode === 'production';
 
-    const isDev = mode === "development";
-    const isProd = mode === "production";
+  const plugins: Configuration['plugins'] = [
+    new HtmlWebpackPlugin({ template: paths.html }),
+    new DefinePlugin({
+      // Значение, которіе задаются при сборки желательно задавать таким образом
+      __PLATFORM__: JSON.stringify(platform),
+    }),
+  ];
 
-    const plugins: Configuration["plugins"] = [
-        new HtmlWebpackPlugin({template: paths.html}),
-    ]
+  if (isDev) {
+    plugins.push(new webpack.ProgressPlugin());
+  }
 
-    if (isDev) {
-        plugins.push(new webpack.ProgressPlugin());
-    };
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      }),
+    );
+  }
 
-    if(isProd) {
-        plugins.push(new MiniCssExtractPlugin({
-            filename: "css/[name].[contenthash:8].css",
-            chunkFilename: "css/[name].[contenthash:8].css",
-          }));
-    };
+  if (analyzer) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
 
-    if(analyzer) {
-        plugins.push(new BundleAnalyzerPlugin())
-    }
-
-    return plugins;
+  return plugins;
 };
