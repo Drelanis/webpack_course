93c8455282abc51280ad4b23a5665165f267d427
Denys <denisbadakaty51@gmail.com>
2024-03-22 19:32:41 +0200

    add build config and init

---
+++ 2_monorepo_module-federation/packages/build-config/buildLoaders.ts
@@ -0,0 +1,89 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { ModuleOptions } from 'webpack';
+import { BuildOptions } from '../types';
+import ReactRefreshTypeScript from 'react-refresh-typescript';
+import { buildBabelLoader } from './babel/buildBabelLoader';
+
+export const buildLoaders = (options: BuildOptions): ModuleOptions['rules'] => {
+  const { mode } = options;
+
+  const isDev = mode === 'development';
+
+  const cssLoaderWithModules = {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+      },
+    },
+  };
+
+  const assetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  };
+
+  const svgrLoader = {
+    test: /\.svg$/i,
+    use: [
+      {
+        loader: '@svgr/webpack',
+        options: {
+          // помогает работать с свг-шками, как с иконками
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              // что бі можно біло задавать цвет в инлайн стилях через колор
+              {
+                name: 'convertColors',
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      cssLoaderWithModules,
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  };
+
+  const tsLoader = {
+    // ts-loader умеет работать с JSX
+    // Если б мы не использовали тайпскрипт: нужен был бы babel-loader
+    exclude: /node_modules/,
+    test: /\.tsx?$/,
+    use: [
+      {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+          }),
+        },
+      },
+    ],
+  };
+
+  const babelLoader = buildBabelLoader(options);
+
+  return [
+    assetLoader,
+    scssLoader,
+    // tsLoader,
+    babelLoader,
+    svgrLoader,
+  ];
+};
