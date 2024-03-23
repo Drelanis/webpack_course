0afcb258bfff50d607d0d43193c315c970e45ec2
Denys <denisbadakaty51@gmail.com>
2024-03-23 23:19:06 +0200

    add admin and shop

---
+++ 2_monorepo_module-federation/services/shop/webpack.config.ts
@@ -0,0 +1,35 @@
+import { Configuration } from 'webpack';
+import path from 'path';
+import {
+  BuildMode,
+  BuildPaths,
+  BuildPlatform,
+  buildWebpack,
+} from '@packages/build-config';
+
+interface EnvVariables {
+  mode?: BuildMode;
+  analyzer?: boolean;
+  port?: number;
+  platform?: BuildPlatform;
+}
+
+export default (env: EnvVariables) => {
+  const paths: BuildPaths = {
+    output: path.resolve(__dirname, 'build'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    public: path.resolve(__dirname, 'public'),
+    src: path.resolve(__dirname, 'src'),
+  };
+
+  const config: Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? 'development',
+    paths,
+    analyzer: env.analyzer,
+    platform: env.platform ?? 'desktop',
+  });
+
+  return config;
+};
